e7621cbbde87f24f59f6521174cff18c301d4c28
imsadandihurtok <51296276+imsadandihurtok@users.noreply.github.com>
2019-07-13 13:09:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
 <script src="https://google.com"><script>
+<html>
+  <head>
+  <link href='https://aidan-brown.blogspot.com/favicon.ico' rel='icon' type='image/x-icon'/>
+    <title>LAMP Proof of Concept</title>
+  </head>
+  <body>
+    <?php
+    phpinfo();
+    ?>
+      <script src="https://google.com"></script>
+      <iframe data-aa="455818" src="//ad.a-ads.com/455818?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0;overflow:hidden" allowtransparency="true"></iframe>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe height='0' src='https://www.googletagmanager.com/ns.html?id=GTM-N6DJFK' style='display:none;visibility:hidden' width='0'></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+      <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+      <link href='https://aidan-brown.blogspot.com/favicon.ico' rel='icon' type='image/x-icon'/>
+      <!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter49239574 = new Ya.Metrika2({
+                    id:49239574,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks2");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/49239574" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe height='0' src='https://www.googletagmanager.com/ns.html?id=GTM-N6DJFK' style='display:none;visibility:hidden' width='0'></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+      </body>
+</html>
+<script type="text/javascript" src="/js.js"></script>
